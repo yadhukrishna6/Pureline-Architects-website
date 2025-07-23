fd1c1e46804510db2eda190e29b14d63e7fdf246
yadhukrishnan <yadhu623871@gmail.com>
2025-07-23 09:07:29 +0530

    fixed nav

--- src/app/view/about-us/about-us.component.ts
+++ src/app/view/about-us/about-us.component.ts
@@ -16,12 +16,295 @@ gsap.registerPlugin(ScrollTrigger);
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss'],
 })
-export class AboutUsComponent implements AfterViewInit {
-  @ViewChild('aboutLeft') aboutLeft!: ElementRef;
-  @ViewChild('aboutRight') aboutRight!: ElementRef;
+export class AboutUsComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild('aboutContainer', { static: true }) aboutContainer!: ElementRef;
+
+  constructor(private elRef: ElementRef) {}
 
   ngAfterViewInit(): void {
-   
-   
+    gsap.registerPlugin(ScrollTrigger);
+
+    const el = this.elRef.nativeElement;
+      const elr = this.aboutContainer.nativeElement;
+ gsap.fromTo(elr,
+      { x: '100vw', opacity: 0 },
+      {
+        x: '0vw',
+        opacity: 1,
+        duration: 1.2,
+        ease: 'power3.out',
+        scrollTrigger: {
+          trigger: el,
+          start: 'top 90%',
+          end: 'top 60%',
+          scrub: false,
+          toggleActions: 'play none none reverse'
+        }
+      });
+    // Banner Big Image
+    gsap.to(el.querySelector('#bannerBigimg'), {
+      duration: 2,
+      yPercent: 150,
+      ease: 'power2.inOut',
+      yoyo: true,
+      repeat: 1,
+      repeatRefresh: true
+    });
+
+    // Banner Big Text
+    gsap.to(el.querySelector('#headingBig'), {
+      duration: 1.5,
+      scale: 2,
+      delay: 0.8,
+      transformOrigin: 'top left',
+      ease: 'power2.inOut',
+      yoyo: true,
+      repeat: 1,
+      repeatRefresh: true
+    });
+
+    // Banner Design Text
+    gsap.to(el.querySelector('#headingSmall'), { delay: 1.2, opacity: 0 });
+    gsap.to(el.querySelector('#headingSmall'), { delay: 2, left: '-100vw' });
+    gsap.to(el.querySelector('#headingSmall'), {
+      delay: 3.6,
+      duration: 1.8,
+      left: 0,
+      opacity: 1
+    });
+
+    // Banner Small Text
+    gsap.to(el.querySelector('#headingText'), { delay: 1.2, opacity: 0 });
+    gsap.to(el.querySelector('#headingText'), { delay: 2, left: '-100vw' });
+    gsap.to(el.querySelector('#headingText'), {
+      delay: 3.6,
+      duration: 1.8,
+      left: 0,
+      opacity: 1
+    });
+
+    // Scroll-triggered animations
+    this.animateScrollTrigger(el);
+  }
+
+  animateScrollTrigger(el: HTMLElement): void {
+    const gsapSet = (selector: string, vars: any) => gsap.set(el.querySelectorAll(selector), vars);
+    const gsapTo = (selector: string, vars: any) => gsap.to(el.querySelectorAll(selector), vars);
+
+    // Company Section
+    gsapSet('.company-section .title, #compDescription', { opacity: 0, y: -200 });
+    gsapTo('.company-section .title, #compDescription', {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      ease: 'power2.inOut',
+      scrollTrigger: {
+        trigger: '.company-section',
+        start: 'top center',
+        end: 'center'
+      }
+    });
+
+    gsapSet('.compy-box', { opacity: 0, scale: 0 });
+    gsapTo('.compy-box', {
+      duration: 1.6,
+      delay: 0.1,
+      opacity: 1,
+      scale: 1,
+      transformOrigin: 'top right',
+      ease: 'power2.inOut',
+      scrollTrigger: {
+        trigger: '.company-section',
+        start: 'top center',
+        end: 'center'
+      }
+    });
+
+    // Designer Section
+    gsapSet('.designer-section .title, #designDescription', { y: -200, opacity: 0 });
+    gsapTo('.designer-section .title, #designDescription', {
+      duration: 1.6,
+      y: 0,
+      opacity: 1,
+      ease: 'power2.inOut',
+      yoyo: true,
+      scrollTrigger: {
+        trigger: '.designer-section',
+        start: 'top center',
+        end: 'center'
+      }
+    });
+
+    gsapSet('.box-border', { opacity: 0, scale: 0.6, rotate: 15 });
+    gsapTo('.box-border', {
+      duration: 1.5,
+      opacity: 1,
+      scale: 1,
+      rotate: 0,
+      ease: 'power.inOut',
+      yoyo: true,
+      scrollTrigger: {
+        trigger: '.designer-section',
+        start: 'top center',
+        end: 'center'
+      }
+    });
+
+    gsapSet('.team1, .team3', { opacity: 0, rotate: 45, scale: 0.5 });
+    gsapTo('.team1, .team3', {
+      duration: 1.6,
+      delay: 0.2,
+      opacity: 1,
+      rotate: 0,
+      scale: 1,
+      ease: 'power2.inOut',
+      transformOrigin: 'top',
+      yoyo: true,
+      scrollTrigger: {
+        trigger: '.designer-section',
+        start: 'top center',
+        end: 'center'
+      }
+    });
+
+    gsapSet('.team2', { opacity: 0, rotate: -45, scale: 0.5 });
+    gsapTo('.team2', {
+      duration: 1.6,
+      delay: 0.2,
+      opacity: 1,
+      rotate: 0,
+      scale: 1,
+      ease: 'power2.inOut',
+      transformOrigin: 'top',
+      yoyo: true,
+      scrollTrigger: {
+        trigger: '.designer-section',
+        start: 'top center',
+        end: 'center'
+      }
+    });
+
+    // Projects Section
+    gsapSet('.projects-section .title', { y: -200, opacity: 0 });
+    gsapTo('.projects-section .title', {
+      duration: 1.6,
+      y: 0,
+      opacity: 1,
+      ease: 'power2.inOut',
+      yoyo: true,
+      scrollTrigger: {
+        trigger: '.projects-section',
+        start: 'top center',
+        end: 'center'
+      }
+    });
+
+    gsapSet('.pbox-left', { opacity: 0, x: -800 });
+    gsapTo('.pbox-left', {
+      duration: 1.6,
+      x: 0,
+      opacity: 1,
+      scale: 1,
+      ease: 'power2.inOut',
+      yoyo: true,
+      scrollTrigger: {
+        trigger: '.projects-section',
+        start: 'top center',
+        end: 'center'
+      }
+    });
+
+    gsapSet('.pbox-right', { x: 500 });
+    gsapTo('.pbox-right', {
+      duration: 1.6,
+      x: 0,
+      ease: 'power2.inOut',
+      yoyo: true,
+      scrollTrigger: {
+        trigger: '.projects-section',
+        start: 'top center',
+        end: 'center'
+      }
+    });
+
+    gsapSet('.project-content', { x: -200, y: -200, opacity: 0 });
+    gsapTo('.project-content', {
+      duration: 1.6,
+      x: 0,
+      y: 0,
+      opacity: 1,
+      delay: 0.2,
+      ease: 'power2.inOut',
+      yoyo: true,
+      scrollTrigger: {
+        trigger: '.projects-section',
+        start: 'top center',
+        end: 'center'
+      }
+    });
+
+    // Testimonial Section
+    gsapSet('.testimonial-section .title', { y: -200, opacity: 0 });
+    gsapTo('.testimonial-section .title', {
+      duration: 1.6,
+      y: 0,
+      opacity: 1,
+      ease: 'power2.inOut',
+      yoyo: true,
+      scrollTrigger: {
+        trigger: '.testimonial-section',
+        start: 'top center',
+        end: 'center'
+      }
+    });
+
+    gsapSet('.left-row', { opacity: 0, xPercent: -100 });
+    gsapTo('.left-row', {
+      duration: 1.6,
+      opacity: 1,
+      xPercent: 0,
+      ease: 'power2.inOut',
+      yoyo: true,
+      scrollTrigger: {
+        trigger: '.testimonial-section',
+        start: 'top center',
+        end: 'center'
+      }
+    });
+
+    gsapSet('.right-row', { opacity: 0, xPercent: 100 });
+    gsapTo('.right-row', {
+      duration: 1.6,
+      opacity: 1,
+      xPercent: 0,
+      ease: 'power2.inOut',
+      yoyo: true,
+      scrollTrigger: {
+        trigger: '.testimonial-section',
+        start: 'top center',
+        end: 'center'
+      }
+    });
+
+    // Contact Section
+    gsapSet('.contact-box, .contact-border', { opacity: 0, scale: 0 });
+    gsapTo('.contact-box, .contact-border', {
+      duration: 1.6,
+      opacity: 1,
+      scale: 1,
+      transformOrigin: 'top right',
+      ease: 'power2.inOut',
+      yoyo: true,
+      scrollTrigger: {
+        trigger: '.contact-us',
+        start: 'top center',
+        end: 'bottom'
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    ScrollTrigger.getAll().forEach(trigger => trigger.kill());
   }
 }
--- src/app/view/homelayout/homelayout.component.ts
+++ src/app/view/homelayout/homelayout.component.ts
@@ -1,9 +1,9 @@
-import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import ScrollToPlugin from 'gsap/ScrollToPlugin';
 
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin); // ← Important!
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin); 
 
 @Component({
   selector: 'app-homelayout',
@@ -12,40 +12,128 @@ gsap.registerPlugin(ScrollTrigger, ScrollToPlugin); // ← Important!
   styleUrl: './homelayout.component.scss'
 })
 export class HomelayoutComponent implements AfterViewInit {
- 
+   @ViewChild('aboutContainer', { static: true }) aboutContainer!: ElementRef;
   constructor(private elRef: ElementRef) { }
+
   ngAfterViewInit(): void {
+  const el = this.elRef.nativeElement;
 
+  const banner = el.querySelector('.banner');
+  const heroSection = el.querySelector('.wrapper');
+  const heroImg = el.querySelector('.hero-img');
+  const card1 = el.querySelector('.card-1');
+  const card2 = el.querySelector('.card-2');
+  const aboutCard = el.querySelector('.about-card');
 
-    console.clear();
+  // 👇 This only triggers banner animations
+  ScrollTrigger.create({
+    trigger: banner,
+    start: 'top center',
+    onEnter: () => {
+      this.animateBanner(el);
+    },
+    onEnterBack: () => {
+      this.animateBanner(el);
+    },
+    once: false
+  });
 
-    gsap.timeline({
-      scrollTrigger: {
-        trigger: this.elRef.nativeElement.querySelector('.wrapper'),
-        start: 'top top',
-        end: '+=150%',
-        pin: true,
-        scrub: true,
-        markers: true
-      }
+  // 👇 Separate GSAP timeline for the wrapper
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: heroSection,
+      start: 'top top',
+      end: '+=300%',
+      pin: true,
+      scrub: true,
+    }
+  })
+    .to(heroImg, {
+      scale: 2,
+      z: 350,
+      transformOrigin: 'center center',
+      ease: 'power1.inOut'
     })
-      .to(this.elRef.nativeElement.querySelector('img'), {
-        scale: 2,
-        z: 350,
-        transformOrigin: 'center center',
-        ease: 'power1.inOut'
-      })
-      .to(
-        this.elRef.nativeElement.querySelector('.section.hero'),
-        {
-          scale: 1.1,
-          transformOrigin: 'center center',
-          ease: 'power1.inOut'
-        },
-        '<'
-      );
-    ScrollTrigger.refresh();
-  }
+    .fromTo(card1, {
+      opacity: 0,
+      y: 100
+    }, {
+      opacity: 1,
+      y: 0,
+      ease: 'power2.out',
+      duration: 1
+    }, '<+0.2')
+    .fromTo(card2, {
+      yPercent: 100
+    }, {
+      yPercent: 0,
+      ease: 'power2.out',
+      duration: 2
+    }, '+=0.5')
+    .to(card1, {
+      xPercent: -170,
+      ease: 'power2.inOut',
+      duration: 2
+    }, '<')
+    .to(card2, {
+      yPercent: -200,
+      ease: 'none',
+      duration: 2
+    })
+    .fromTo(aboutCard,
+      { xPercent: 100, opacity: 0 },
+      {
+        xPercent: 0,
+        opacity: 1,
+        ease: 'power2.out',
+        duration: 2
+      },
+      '<+0.5'
+    );
 
+  ScrollTrigger.refresh();
+}
+animateBanner(el: HTMLElement) {
+  gsap.set(el.querySelector('#headingSmall'), { left: 0, opacity: 1 });
+  gsap.set(el.querySelector('#headingText'), { left: 0, opacity: 1 });
+
+  gsap.to(el.querySelector('#bannerBigimg'), {
+    duration: 2,
+    yPercent: 150,
+    ease: 'power2.inOut',
+    yoyo: true,
+    repeat: 1,
+    repeatRefresh: true
+  });
+
+  gsap.to(el.querySelector('#headingBig'), {
+    duration: 1.5,
+    scale: 2,
+    delay: 0.8,
+    transformOrigin: 'top left',
+    ease: 'power2.inOut',
+    yoyo: true,
+    repeat: 1,
+    repeatRefresh: true
+  });
+
+  gsap.to(el.querySelector('#headingSmall'), { delay: 1.2, opacity: 0 });
+  gsap.to(el.querySelector('#headingSmall'), { delay: 2, left: '-100vw' });
+  gsap.to(el.querySelector('#headingSmall'), {
+    delay: 3.6,
+    duration: 1.8,
+    left: 0,
+    opacity: 1
+  });
+
+  gsap.to(el.querySelector('#headingText'), { delay: 1.2, opacity: 0 });
+  gsap.to(el.querySelector('#headingText'), { delay: 2, left: '-100vw' });
+  gsap.to(el.querySelector('#headingText'), {
+    delay: 3.6,
+    duration: 1.8,
+    left: 0,
+    opacity: 1
+  });
+}
 
 }
